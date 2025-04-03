3110f241663a7d68ec4450a61be4229b464706b6
Davikechner <kechner.oliveira@estudante.ifro.edu.br>
2025-04-03 11:26:26 -0400

    atividade_01 03/04/2025

--- Comandos.js
+++ Comandos.js
@@ -1,2 +1,31 @@
 console.log("Hello World!"); //FUNÇÂO PARA IMPRIMIR
 
+var valor = 20;
+valor.toFixed(2);  "20.00" //VAI ADICIONAR APENAS 2 CASAS DECIMAIS
+
+var ativo = true
+ativo.toSting(); "True" //VAI RETORNAR EM STRING
+
+var nome = "Hello World";
+nome.toLowerCase(); //VAI MUDAR AS PALAVRAS PRA MINUSCULO
+
+var estado = ["Rondônia", "Mato Grosso"]
+estado [0] //PARA ACESSAR
+
+var linguagens = ["JavaScript","PHP","JAVA"]
+linguagens [1] = "Python" //IRA MUDAR A POSIÇÂO SELECIONADA PARA OQ VC QUER
+
+var linguagens = ["JavaScript","PHP","JAVA"]
+console.log(linguagens.length); //PARA SABER QUANDO ELEMENTOS TEM DENTRO DO ARRAY
+
+var nome ;
+console.log(nome) //VAI IMPRIMIR UNDEFINED(a variavel n foi definida)
+
+var telefones = [
+    "(11) 98899 - 8787",
+    "(69) 99257 - 7045",
+    "(91) 93453 - 9921"
+] //PARA N PRECISAR CRIAR VARIAS VARIAVEIS COM NOMES DIFERENTES(ex: telefone1,telefone2)
+
+const aula = "JavaScript";
+aula = "JS"; //A VARIAVEL SERA IMUTAVEL(nao podera ser atualizada)
---
+++ atividade_01.js
@@ -0,0 +1,35 @@
+// //-01
+let nome = "Davi Kechner";
+let idade = "15";
+
+console.log("Olá meu nome é",nome,"e eu tenho",idade, "anos")
+
+
+
+//-02
+let string = "Eu Estudo no If";
+texto = string.toLowerCase();
+console.log(texto);
+
+texto = string.toLocaleUpperCase();
+console.log(texto);
+
+
+
+//-03
+let num = "2";
+let number = 8
+
+let texto1 = (typeof num);
+console.log(texto1);
+
+let texto2 = (typeof number);
+console.log(texto2);
+
+
+
+//-04
+let peso = 64
+let altura = 1.84
+let calculo = peso / (altura + altura);
+console.log(calculo)
