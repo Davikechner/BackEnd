c998c0c900257b21330dd5ab4934013298b7b5eb
Davikechner <kechner.oliveira@estudante.ifro.edu.br>
2025-07-10 11:38:55 -0400

    T_T

--- Servico imc/calculadoraimc.js
+++ Servico imc/calculadoraimc.js
@@ -3,4 +3,38 @@ function efetuarCalculoImc(peso, altura) {
     return imc.toFixed(2);
 }
 
+function retornarStatusIMC(imc)
+{
+    let status;
+    if(imc < 18.5) {
+        status = "Abaixo do Peso";
+    }
+
+    else if(imc >= 18.5 && imc < 24.9) {
+        status = "Peso Normal";
+    }
+
+    else if(imc >= 24.9 && imc < 30) {
+        status = "Acima do Peso";
+    }
+
+    else {
+        status = "Obeso";
+    }
+
+    return status
+}
+
+function validarParametro(parametro) {
+    if(isNaN(parametro))  {
+        return false;
+    }
+
+    else {
+        return true;
+    }
+}
+
 exports.efetuarCalculoImc = efetuarCalculoImc;
+exports.retornarStatusIMC = retornarStatusIMC;
+exports.validarParametro = validarParametro
--- indeximc.js
+++ indeximc.js
@@ -5,9 +5,22 @@ const calculaImc = require('./Servico imc/calculadoraimc');
 app.get('/', (req, res) => {
     let peso = req.query.peso;
     let altura = req.query.altura;
-    let imc = calculaImc.efetuarCalculoImc(peso, altura);
 
-    res.json({imc: imc});
+
+    if (calculaImc.validarParametro(req.query.peso) && calculaImc.validarParametro(req.query.altura)) {
+        let imc = calculaImc.efetuarCalculoImc(peso, altura);
+        let status = calculaImc.retornarStatusIMC(imc);
+
+
+        let json = { imc: imc, status: status };
+        res.json(json);
+    }
+
+    else {
+        res.status(400).json({"Erro" : "Peso ou altura invalida!"})
+    }
+        
+
 });
 
 app.listen(8080, () => {
